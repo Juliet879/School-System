05cbcf3c9f92925884b2ae6bfe618fbbfccf0861
Juliet879 <gisembajuliett@gmail.com>
2021-08-09 19:31:28 +0300

    modified the static files

--- School_System/student/forms.py
+++ School_System/student/forms.py
@@ -7,4 +7,7 @@ class StudentRegistrationForm(forms.ModelForm):
         model = Student
         # tells the database it wants all the data in the database for chosen fields you put the fields in a tuple
         fields = "__all__"     
-        
+        widgets = {
+            "first_name":forms.TextInput(attrs={'class':"form-control",'style':"width:70%"})
+        }
+
